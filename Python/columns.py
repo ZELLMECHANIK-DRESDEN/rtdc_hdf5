e177857595133ef029187541cad648f265723887
paulmueller <paulmueller@users.noreply.github.com>
2017-08-07 11:41:42 +0200

    doc update

--- Python/columns.py
+++ Python/columns.py
@@ -1,8 +1,10 @@
-columns = [
+# These are columns that contain a scalar value for each event.
+# Other valid columns are "image", "contour", and "trace".
+_columns = [
    ["area_cvx", "Convex area [px]"],
    ["area_msd", "Measured area [px]"],
    ["area_ratio", "Convex to measured area ratio"],
-   # This is the convex area per definition
+   # area_um is the convex area per definition
    ["area_um", "Area [µm²]"],
    ["aspect", "Aspect ratio of bounding box"],
    ["bright_avg", "Brightness average within contour [a.u.]"],
@@ -10,15 +12,16 @@ columns = [
    ["circ", "Circularity"],
    ["deform", "Deformation"],
    ["emodulus", "Young's Modulus [kPa]"],
-   # Is this the area of the first or the highest peak?
+   # fl*_area, fl*_pos, and fl*_width values correspond to the
+   # object for which the contour was found. For high concentrations,
+   # these values could be error-prone due to the assignment from
+   # false objects. 
    ["fl1_area", "FL-1 area of peak [a.u.]"],
-   # Set to nan if there is only one peak?
+   # fl1_dist is set to zero if there is only one peak
    ["fl1_dist", "FL-1 distance between two first peaks [µs]"],
    ["fl1_max", "FL-1 maximum [a.u.]"],
    ["fl1_npeaks", "FL-1 number of peaks"],
-   # Is this the position of the first or the highest peak?
    ["fl1_pos", "FL-1 position of peak [µs]"],
-   # Is this the width of the first or the highest peak?
    ["fl1_width", "FL-1 width [µs]"],
    ["fl2_area", "FL-2 area of peak [a.u.]"],
    ["fl2_dist", "FL-2 distance between two first peaks [µs]"],
@@ -34,7 +37,7 @@ columns = [
    ["fl3_width", "FL-3 width [µs]"],
    ["frame", "Video frame number"],
    # The inertia ratios of the event contours are defined by the
-   # second moments of area. 
+   # central second order moments of area. 
    ["inert_ratio_cvx", "Inertia ratio of convex contour sqrt(m20/m02)"],
    ["inert_ratio_raw", "Inertia ratio of raw contour sqrt(m20/m02)"],
    ["index", "Event index"],
@@ -48,4 +51,3 @@ columns = [
    ["time", "Event time [s]"],
    ["volume", "Volume [µm³]"],
    ]
-
